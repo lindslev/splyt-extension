7fc263690af823edaca093b30d8966da40964ad1
Lindsay <ilinds4@aol.com>
2015-02-06 19:26:14 -0500

    can add songs to specific playlist

--- extension/src/browser_action/browser_action.js
+++ extension/src/browser_action/browser_action.js
@@ -13,32 +13,54 @@ angular.module('splytApp', [])
     $scope.isLoggedIn = currentPage.isLoggedIntoApp();
     $scope.currentUser = currentPage.currentUser();
     var token = currentPage.getToken();
+    $scope.clickedAddSong = false;
+
+    if($scope.currentUser) {
+      var req = {
+        method: 'GET',
+        url: 'http://192.168.1.121:9000/api/users/' + $scope.currentUser._id + '/playlists',
+        headers: {
+          'authorization': "Bearer " + token
+        }
+      }
+      $http(req).success(function(playlists){
+        $scope.userPlaylists = [];
+        playlists.forEach(function(list){ if(list['friend_stream'] == false) $scope.userPlaylists.push(list); })
+      })
+    }
 
     $scope.addSong = function(song) {
       console.log('adding song', song);
-      console.log('document.cookie', document.cookie)
+      $scope.selectedSong = song;
+      $scope.clickedAddSong = true;
+    }
+
+    $scope.addPlaylist = function(playlist) {
+      console.log('adding playlist', playlist);
+    }
+
+    $scope.login = function() {
+      window.open('http://192.168.1.121:9000/auth/google', '_blank');
+    }
+
+    $scope.addToPlaylist = function(playlist) {
+      $scope.clickedAddSong = false;
+      //add new song to server
       var req = {
         method: 'POST',
-        url: 'http://192.168.1.121:9000/api/users/addSong/' + $scope.currentUser._id +'/playlist/'+ $scope.currentUser.playlist[0],
+        url: 'http://192.168.1.121:9000/api/users/addSong/' + $scope.currentUser._id + '/playlist/' + playlist._id,
         headers: {
           'authorization': "Bearer " + token
         },
-        data: song
+        data: $scope.selectedSong
       }
-
+      console.log('testing...'),
       $http(req)
         .success(function(data){
           console.log('data back from server', data);
         })
-      // $http.post('http://192.168.1.121:9000/api/users/addSong/' + $scope.currentUser._id +'/playlist/'+ $scope.currentUser.playlist[0], song)
-      //   .success(function(data){
-      //     console.log(data);
-      //   })
-    }
-    $scope.addPlaylist = function(playlist) {
-      console.log('adding playlist', playlist);
-    }
-    $scope.login = function() {
-      window.open('http://192.168.1.121:9000/auth/google', '_blank');
+        .error(function(err){
+          console.log('err', err);
+        })
     }
   });
